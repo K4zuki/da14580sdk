9961c2777417a81bf1e5125e6602beb85f51480e
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2016-10-24 20:44:01 +0900

    correct define macro's

--- include/pin_names.h
+++ include/pin_names.h
@@ -108,12 +108,10 @@ typedef enum {
   P28_MODE_ = 0x56,      /* P28  Mode  Register  */
   P29_MODE_ = 0x58,      /* P29  Mode  Register  */
 
-  P01_PADPWR_CTRL_ = 0x70, /*  Ports  0  and  1  Output  Power
-                                           Control  Register  */
-  P2_PADPWR_CTRL_ = 0x72,  /*  Port  2  Output  Power  Control
-                                           Register      */
-  P3_PADPWR_CTRL_ = 0x74,  /*  Port  3  Output  Power  Control
-                                           Register      */
+  P01_PADPWR_CTRL_ =
+      0x70, /*  Ports  0  and  1  Output  Power Control  Register  */
+  P2_PADPWR_CTRL_ = 0x72, /*  Port  2  Output  Power  Control Register      */
+  P3_PADPWR_CTRL_ = 0x74, /*  Port  3  Output  Power  Control Register      */
 
   P3_DATA_ = 0x80,       /* P3 Data input / output register */
   P3_SET_DATA_ = 0x82,   /* P3 Set  port  pins  register */
@@ -134,99 +132,52 @@ typedef enum {
 #define PORT2_BASE 0x50003040
 #define PORT3_BASE 0x50003080
 
-#define P0_DATA_RW          ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P0_DATA_       ))
-#define P0_SET_DATA         ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P0_SET_DATA_   ))
-#define P0_RESET_DATA       ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P0_RESET_DATA_ ))
-#define P00_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P00_MODE_      ))
-#define P01_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P01_MODE_      ))
-#define P02_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P02_MODE_      ))
-#define P03_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P03_MODE_      ))
-#define P04_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P04_MODE_      ))
-#define P05_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P05_MODE_      ))
-#define P06_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P06_MODE_      ))
-#define P07_MODE            ((uint32_t*) (PORT0_BASE |
-                              (uint32_t)P07_MODE_      ))
-#define P1_DATA_RW          ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P1_DATA_       ))
-#define P1_SET_DATA         ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P1_SET_DATA_   ))
-#define P1_RESET_DATA       ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P1_RESET_DATA_ ))
-#define P10_MODE            ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P10_MODE_      ))
-#define P11_MODE            ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P11_MODE_      ))
-#define P12_MODE            ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P12_MODE_      ))
-#define P13_MODE            ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P13_MODE_      ))
-#define P14_MODE            ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P14_MODE_      ))
-#define P15_MODE            ((uint32_t*) (PORT1_BASE |
-                              (uint32_t)P15_MODE_      ))
-#define P2_DATA_RW          ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P2_DATA_       ))
-#define P2_SET_DATA         ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P2_SET_DATA_   ))
-#define P2_RESET_DATA       ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P2_RESET_DATA_ ))
-#define P20_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P20_MODE_      ))
-#define P21_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P21_MODE_      ))
-#define P22_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P22_MODE_      ))
-#define P23_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P23_MODE_      ))
-#define P24_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P24_MODE_      ))
-#define P25_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P25_MODE_      ))
-#define P26_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P26_MODE_      ))
-#define P27_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P27_MODE_      ))
-#define P28_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P28_MODE_      ))
-#define P29_MODE            ((uint32_t*) (PORT2_BASE |
-                              (uint32_t)P29_MODE_      ))
-#define P01_PADPWR_CTRL     ((uint32_t*) (GPIO_BASE  |
-                              (uint32_t)P01_PADPWR_CTRL))
-#define P2_PADPWR_CTRL      ((uint32_t*) (GPIO_BASE  |
-                              (uint32_t)P2_PADPWR_CTRL_))
-#define P3_PADPWR_CTRL      ((uint32_t*) (GPIO_BASE  |
-                              (uint32_t)P3_PADPWR_CTRL_))
-#define P3_DATA_RW          ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P3_DATA_       ))
-#define P3_SET_DATA         ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P3_SET_DATA_   ))
-#define P3_RESET_DATA       ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P3_RESET_DATA_ ))
-#define P30_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P30_MODE_      ))
-#define P31_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P31_MODE_      ))
-#define P32_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P32_MODE_      ))
-#define P33_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P33_MODE_      ))
-#define P34_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P34_MODE_      ))
-#define P35_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P35_MODE_      ))
-#define P36_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P36_MODE_      ))
-#define P37_MODE            ((uint32_t*) (PORT3_BASE |
-                              (uint32_t)P37_MODE_      ))
+#define P0_DATA_RW ((uint32_t *)(PORT0_BASE | (uint32_t)P0_DATA_))
+#define P0_SET_DATA ((uint32_t *)(PORT0_BASE | (uint32_t)P0_SET_DATA_))
+#define P0_RESET_DATA ((uint32_t *)(PORT0_BASE | (uint32_t)P0_RESET_DATA_))
+#define P00_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P00_MODE_))
+#define P01_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P01_MODE_))
+#define P02_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P02_MODE_))
+#define P03_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P03_MODE_))
+#define P04_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P04_MODE_))
+#define P05_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P05_MODE_))
+#define P06_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P06_MODE_))
+#define P07_MODE ((uint32_t *)(PORT0_BASE | (uint32_t)P07_MODE_))
+#define P1_DATA_RW ((uint32_t *)(PORT1_BASE | (uint32_t)P1_DATA_))
+#define P1_SET_DATA ((uint32_t *)(PORT1_BASE | (uint32_t)P1_SET_DATA_))
+#define P1_RESET_DATA ((uint32_t *)(PORT1_BASE | (uint32_t)P1_RESET_DATA_))
+#define P10_MODE ((uint32_t *)(PORT1_BASE | (uint32_t)P10_MODE_))
+#define P11_MODE ((uint32_t *)(PORT1_BASE | (uint32_t)P11_MODE_))
+#define P12_MODE ((uint32_t *)(PORT1_BASE | (uint32_t)P12_MODE_))
+#define P13_MODE ((uint32_t *)(PORT1_BASE | (uint32_t)P13_MODE_))
+#define P14_MODE ((uint32_t *)(PORT1_BASE | (uint32_t)P14_MODE_))
+#define P15_MODE ((uint32_t *)(PORT1_BASE | (uint32_t)P15_MODE_))
+#define P2_DATA_RW ((uint32_t *)(PORT2_BASE | (uint32_t)P2_DATA_))
+#define P2_SET_DATA ((uint32_t *)(PORT2_BASE | (uint32_t)P2_SET_DATA_))
+#define P2_RESET_DATA ((uint32_t *)(PORT2_BASE | (uint32_t)P2_RESET_DATA_))
+#define P20_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P20_MODE_))
+#define P21_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P21_MODE_))
+#define P22_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P22_MODE_))
+#define P23_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P23_MODE_))
+#define P24_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P24_MODE_))
+#define P25_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P25_MODE_))
+#define P26_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P26_MODE_))
+#define P27_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P27_MODE_))
+#define P28_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P28_MODE_))
+#define P29_MODE ((uint32_t *)(PORT2_BASE | (uint32_t)P29_MODE_))
+#define P01_PADPWR_CTRL ((uint32_t *)(GPIO_BASE | (uint32_t)P01_PADPWR_CTRL))
+#define P2_PADPWR_CTRL ((uint32_t *)(GPIO_BASE | (uint32_t)P2_PADPWR_CTRL_))
+#define P3_PADPWR_CTRL ((uint32_t *)(GPIO_BASE | (uint32_t)P3_PADPWR_CTRL_))
+#define P3_DATA_RW ((uint32_t *)(PORT3_BASE | (uint32_t)P3_DATA_))
+#define P3_SET_DATA ((uint32_t *)(PORT3_BASE | (uint32_t)P3_SET_DATA_))
+#define P3_RESET_DATA ((uint32_t *)(PORT3_BASE | (uint32_t)P3_RESET_DATA_))
+#define P30_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P30_MODE_))
+#define P31_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P31_MODE_))
+#define P32_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P32_MODE_))
+#define P33_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P33_MODE_))
+#define P34_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P34_MODE_))
+#define P35_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P35_MODE_))
+#define P36_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P36_MODE_))
+#define P37_MODE ((uint32_t *)(PORT3_BASE | (uint32_t)P37_MODE_))
 
-#endif  //_PIN_NAMES_H_
+#endif  //_PIN_NAMES_H_ //NOLINT
